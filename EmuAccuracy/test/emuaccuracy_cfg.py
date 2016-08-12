85fe808f40af95fc3366de371c9adda0f6c41360
Jia Fu Low <jia.fu.low@cern.ch>
2016-08-11 21:20:50 -0400

    Third commit

--- EmuAccuracy/test/emuaccuracy_cfg.py
+++ EmuAccuracy/test/emuaccuracy_cfg.py
@@ -8,9 +8,10 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
-    fileNames = cms.untracked.vstring(
-        'file:myfile.root'
-    )
+    #fileNames = cms.untracked.vstring(
+    #    'file:myfile.root'
+    #)
+    fileNames = cms.untracked.vstring('/store/data/Run2016F/SingleMuon/RAW/v1/000/278/018/00000/A015A25D-A058-E611-9DF0-02163E011E0F.root'),
 )
 
 from L1TMuonEndCapStudies.EmuAccuracy.emuaccuracy_cfi import emuaccuracy
