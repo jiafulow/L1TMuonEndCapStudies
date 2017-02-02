9e6de528d60c33ebd711be3bbf5f96d8a2de97bb
Jia Fu Low <jia.fu.low@cern.ch>
2017-02-02 10:54:09 -0500

    More update to condor stuff [2]

--- RPCIntegrationSep2016/condor/run_all_20161220.sh
+++ RPCIntegrationSep2016/condor/run_all_20161220.sh
@@ -4,4 +4,6 @@
 
 # Testing
 #./submit.sh NoAnalyzer SingleMu_Pt1To1000_FlatRandomOneOverPt_foo singlemu emtf 2016 1
- 
+
+# hadd
+#hadd -f histos_add_20161220.root blt_projects/NoAnalyzer/SingleMu_Pt1To1000_FlatRandomOneOverPt/out/histos_*.root
